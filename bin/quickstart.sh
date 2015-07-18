da5bf15903299c4f7ce8d279f1e1bec8ce63bca8
James Gorlick <jgorlick@basho.com>
2015-07-17 18:11:57 -0700

    added robust retry semantics around areas of the quickstart that often take time to reach a good, workable state.

--- bin/quickstart.sh
+++ bin/quickstart.sh
@@ -7,6 +7,22 @@ function assert_exit () {
         exit $EXIT_CODE
     fi
 }
+function retry_bdp_service_start() {
+    local SERVICE_GROUP=$1
+    local SERVICE_CONFIG=$2
+    let EXPECTED_STATUS_LINES=$TARGET_VM_COUNT+1
+    local RETRIES=3
+    while [[ $RETRIES > 0 ]]; do
+        ./bin/bdp_service_control.sh start "$SERVICE_GROUP" "$SERVICE_CONFIG" >bdp_service_start.log 2>&1
+        assert_exit "starting $SERVICE_CONFIG"
+        STATUS_LINES=$(./bin/bdp_service_status.sh |grep "$SERVICE_CONFIG" |wc -l)
+        if [[ "$STATUS_LINES" -ge "$EXPECTED_STATUS_LINES" ]]; then
+            RETRIES=0
+        fi
+    done
+    cat bdp_service_start.log
+    rm bdp_service_start.log
+}
 
 echo "create the riak cluster"
 ./bin/riak_cluster_create.sh
@@ -18,11 +34,30 @@ echo "create the bdp cluster"
 ./bin/bdp_cluster_create.sh
 assert_exit "create bdp cluster"
 echo "verify the bdp cluster is established"
-./bin/bdp_service_status.sh
-assert_exit "verify bdp cluster, likely insufficient up nodes"
+RETRIES=60 #<< experiencing 25-30s periods of 
+while [[ $RETRIES > 0 ]]; do
+    # NOTE: intentionally freshly loggin output to only emit the last run below
+    ./bin/bdp_service_status.sh >bdp_service_status.log 2>&1
+    EXIT_CODE=$?
+    if [[ $EXIT_CODE == 0 ]]; then
+        RETRIES=0
+        echo ""
+    else
+        let RETRIES-=1
+        printf "."
+        sleep 1
+    fi
+done
+cat bdp_service_status.log
+rm bdp_service_status.log
+if [[ $EXIT_CODE != 0 ]]; then
+    (exit $EXIT_CODE)
+    assert_exit "verify bdp cluster, likely insufficient up nodes"
+fi
+
 echo "create bdp core service configurations"
-for i in `ls bin_service_config_create.sh`;do
-    .$i
+for i in `ls bin/*service_config_create.sh`;do
+    ./$i
     assert_exit "creating service config $i"
 done
 echo "verify bdp service configurations were created"
@@ -32,14 +67,10 @@ echo "setting up bucket properties for spark"
 ./bin/spark_riak_bucket_create.sh
 assert_exit "setting bucket properties for spark"
 echo "starting my-redis, my-cache-proxy, my-spark-master and my-spark-worker"
-./bin/bdp_service_control.sh start my-cache-group my-redis
-assert_exit "starting my-redis"
-./bin/bdp_service_control.sh start my-cache-group my-cache-proxy
-assert_exit "starting my-cache-proxy"
-./bin/bdp_service_control.sh start my-analysis-group my-spark-master
-assert_exit "starting my-spark-master"
-./bin/bdp_service_control.sh start my-analysis-group my-spark-worker
-assert_exit "starting my-spark-worker"
+retry_bdp_service_start "my-cache-group" "my-redis"
+retry_bdp_service_start "my-cache-group" "my-cache-proxy"
+retry_bdp_service_start "my-analysis-group" "my-spark-master"
+retry_bdp_service_start "my-analysis-group" "my-spark-worker"
 echo "verify bdp services were started"
 ./bin/bdp_service_status.sh
 assert_exit "verify bdp cluster, likely insufficient up nodes"
